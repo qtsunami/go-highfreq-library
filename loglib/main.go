f58bb0bc868bd61c36c5dcf1313bc58891a26dcb
qtsunami <xuyanlu0424@163.com>
2023-03-15 15:45:55 +0800

    logrus example

--- loglib/main.go
+++ loglib/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"io"
+	"os"
+)
 
 func main() {
-	fmt.Println("HHH")
+	logFile := "log.txt"
+	f, err := os.OpenFile(logFile, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		fmt.Println("Failed to create logfile" + logFile)
+		panic(err)
+	}
+	defer f.Close()
+
+	log := &logrus.Logger{
+		Out:       io.MultiWriter(f, os.Stdout),
+		Level:     logrus.DebugLevel,
+		Formatter: &logrus.TextFormatter{},
+	}
+
+	log.SetReportCaller(true)
+
+	log.Info("Info Level")
+	log.Warn("Warn Level")
+	log.Error("Error Level")
+	log.Fatal("Fatal Level")
 }
